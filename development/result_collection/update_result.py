d6c6b62eedd7ab01ce60c1c17c76724dab816856
TysonW <wukwanyutyson@gmail.com>
2020-02-20 08:50:10 +0800

    Poisson probability analysis

--- development/result_collection/update_result.py
+++ development/result_collection/update_result.py
@@ -5,14 +5,15 @@ import os
 
 def update_result():
     os.chdir('/Users/TysonWu/dev/odds-crawl-app/odds-crawl-app/development/')
-    
+
     all_matches = pd.read_csv('data_collection/data/match_data.csv')
     current_result_data = pd.read_csv('result_collection/match_corner_result.csv')
-    
+
     updated_result_data = all_matches.merge(
         current_result_data[['event_id','result_corner']], how='outer', on='event_id')
-    
+
     updated_result_data.to_csv('result_collection/match_corner_result.csv', index=False)
 
 if __name__ == "__main__":
     update_result()
+    
