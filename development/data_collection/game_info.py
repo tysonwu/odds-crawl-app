cffe474f16c3386cdd3dd3be5f84544539d6be14
TysonW <wukwanyutyson@gmail.com>
2020-02-24 11:42:39 +0800

    No longer using game_info

--- development/data_collection/game_info.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-path_dir = '/Users/TysonWu/dev/odds-crawl-app/odds-crawl-app/development/data_collection/data/' #save dir
-
-class game_data:
-    def __init__(self):
-        self.path_dir = path_dir
